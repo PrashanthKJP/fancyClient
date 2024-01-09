ef12747844dbcad3473bad2a83fa96c03afd1f6b
PrashanthKJP <prashanthprash969@gmail.com>
2024-01-09 13:33:36 +0530

    ip address changed

--- src/service/helper.js
+++ src/service/helper.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = "https://fancy-number.onrender.com";
-export const BASE_URL = "http://localhost:8888";
+// export const BASE_URL = "http://localhost:8888";
+export const BASE_URL = "http://134.209.147.163";
